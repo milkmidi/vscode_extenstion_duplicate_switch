07e008c7bfb952cb6956b2336d395e62febceb1c
milkmidi <milkmidi@medialand.tw>
2017-04-14 12:48:26 +0800

    add text function

--- src/extension.ts
+++ src/extension.ts
@@ -3,9 +3,8 @@ import * as vscode from 'vscode';
 const config = vscode.workspace.getConfiguration('duplicateLineSwitchWord') || {};
 const WORD_ARR: string[] = config["pattern"];
 export function activate(context: vscode.ExtensionContext) {
-    console.log('Congratulations, your extension "dulpicate" is now active!');
+    console.log('Congratulations, your extension "duplicate switch" is now active!');
     let disposable = vscode.commands.registerCommand('extension.duplicateLineSwitchWord', () => {
-
         const editor = vscode.window.activeTextEditor;
         const document = editor.document;
         if (editor.selection.isEmpty) {
@@ -19,16 +18,15 @@ export function activate(context: vscode.ExtensionContext) {
     });
     context.subscriptions.push(disposable);
 }
-export function replace(text: string) {
+export function replace(text: string) :string {
     for (var key of WORD_ARR) {
         let keys = key.split(',');
-
         for (var i = 0; i < keys.length; i++) {
             var value = keys[i];
             if( text.indexOf(value) !== -1 ){
                 return '\n'+ text.replace(value, keys[~~!i]);
             }
-        }                
+        }        
     }
     return '\n' + text;
 }
--- test/extension.test.ts
+++ test/extension.test.ts
@@ -2,27 +2,22 @@
 // Note: This example test is leveraging the Mocha test framework.
 // Please refer to their documentation on https://mochajs.org/ for help.
 //
-
-// The module 'assert' provides assertion methods from node
 import * as assert from 'assert';
-
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
 import * as vscode from 'vscode';
 import * as myExtension from '../src/extension';
 
-// Defines a Mocha test suite to group tests of similar kind together
 suite("Extension Tests", () => {
     const config = vscode.workspace.getConfiguration('duplicateLineSwitchWord');
     const WORD_ARR: string[] = config["pattern"];
-    // Defines a Mocha unit test
-    test("should config pattern is Array", () => {
+    test("should return true when config pattern is Array", () => {
         assert.equal(true, Array.isArray(WORD_ARR));
     });
-
     test("myExtension replace", () => {
         assert.equal(myExtension.replace("width:50px"), "\nheight:50px");
         assert.equal(myExtension.replace("height:50px"), "\nwidth:50px");
         assert.equal(myExtension.replace("myClass.addEventListener"), "\nmyClass.removeEventListener");
+        assert.equal(myExtension.replace("myClass.removeEventListener"), "\nmyClass.addEventListener");
+        assert.equal(myExtension.replace("{top:10rem}"), "\n{bottom:10rem}");
+        assert.equal(myExtension.replace("{bottom:10rem}"), "\n{top:10rem}");
     });
 });
